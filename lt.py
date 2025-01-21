859bd4f6e0a244385fe41719333486fc5f5dee0b
Aayushi-24 <patilaayushi8824@gmail.com>
2025-01-21 10:39:41 -0800

    Add files via upload

---
+++ lt.py
@@ -0,0 +1,11 @@
+class A:
+    def __init__ (self.a):
+        self.a=a 
+        def __lt__(self.o):
+            if(self.a<o.a):
+                print("ob1 is lt ob2")
+            else:
+                print("ob2 is lt ob1")
+                ob1 =A(2)
+                ob2 =A(3)
+               (ob1 > ob2)
---
+++ range odd nd even.py
@@ -0,0 +1,5 @@
+lower=int(input("enter the lower of x="))
+upper=int(input("enter the upper of y="))
+for i in range (lower,upper+1):
+    if (i%2==0):
+        print(i)
